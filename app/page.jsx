0a7490d51cb74642a92e285a661a81237a52cdb2
Peterged <petergedon17@gmail.com>
2024-03-31 00:00:34 +0800

    setup: Added base colors to config

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,16 @@
+import React from 'react';
+
+// Import Components
+import Sidebar from "@/components/containers/Sidebar";
+import Home from '@/components/containers/Home';
+
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="flex p-3 bg-potrello-white-200">
+      <Sidebar />
+        <Home />
+    </div>
+  );
 }
 
 export default page;
