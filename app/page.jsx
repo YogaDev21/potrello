f7e7bf17ec82bffe1e8f7928c92b4d28a60408d3
yogadev21 <yogadev2006@gmail.com>
2024-03-31 10:40:23 +0800

    feat: add pomodoro timer
    adding pomodoro timer to project

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 // Import Components
 import Sidebar from "@/components/containers/Sidebar";
-import Home from '@/components/containers/Home';
+import Home from "@/components/containers/Home";
 
 function page() {
   return (
     <div className="flex p-3 bg-potrello-white-200">
       <Sidebar />
-        <Home />
+      <Home />
     </div>
   );
 }
--- components/containers/Sidebar.jsx
+++ components/containers/Sidebar.jsx
@@ -1,5 +1,4 @@
-"use client"
-
+"use client";
 
 import React, { useState } from "react";
 
@@ -9,29 +8,28 @@ import { LuUser2 } from "react-icons/lu";
 
 // Initialize Variables
 const buttonList = {
-    Dashboard: <PiHouseSimple className="fill-gray-400 size-6" />,
-    Profile: <LuUser2 className="stroke-gray-400 size-6" />,
-}
+  Dashboard: <PiHouseSimple className="fill-gray-400 size-6" />,
+  Profile: <LuUser2 className="stroke-gray-400 size-6" />,
+};
 
 export default function Sidebar() {
-    const [selectedButton, setSelectedButton] = useState(null);
+  const [selectedButton, setSelectedButton] = useState(null);
 
-    return (
-        <div className="bg-potrello-white-100 shadow-lg shadow-gray-300 min-h-screen w-12 p-1 pt-2 rounded-xl">
-            <div className="flex flex-col items-center w-full h-full ">
-
-                {Object.keys(buttonList).map((value) => (
-                    <button
-                        key={value}
-                        className={`flex py-2 text-white font-normal ${selectedButton === value ? "bg-potrello-blue-1000" : ""}`}
-                        onClick={() => setSelectedButton(value)}
-                    >
-                        {buttonList[value]}
-                    </button>
-                ))}
-                
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-potrello-white-100 shadow-lg shadow-gray-300 min-h-screen w-12 p-1 pt-2 rounded-xl">
+      <div className="flex flex-col items-center w-full h-full ">
+        {Object.keys(buttonList).map((value) => (
+          <button
+            key={value}
+            className={`flex py-2 text-white font-normal ${
+              selectedButton === value ? "bg-potrello-blue-1000" : ""
+            }`}
+            onClick={() => setSelectedButton(value)}
+          >
+            {buttonList[value]}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
-
