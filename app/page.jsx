3a5e9dd849373424c30bd852f0e9b8781cd19419
Peterged <petergedon17@gmail.com>
2024-04-15 17:12:09 +0800

    Expecto Reseto

--- app/page.jsx
+++ app/page.jsx
@@ -3,30 +3,12 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
-// Import Utility Functions
-import randomNumber from '@/src/utils/randomNumber';
-import getRandFile from '@/src/utils/getRandomFile';
-
-// Import Components
-import Sidebar from "@/src/components/organisms/Sidebar";
-import Home from '@/src/components/templates/Home';
+import Home from '@/src/components/Home';
 
 function page() {
-    const [imageFilename, setImageFilename] = useState("");
-
-    useEffect(() => {
-        const fetchImage = async () => {
-            const fileList = await getRandFile('public/img/');
-            const totalChoices = fileList.length;
-            const fileName = fileList[randomNumber(0, totalChoices - 1)];
-            setImageFilename(fileName);
-        }
-
-        fetchImage();
-    }, []);
 
     return (
-        <div id="page-container" className="min-h-screen grid bg-no-repeat bg-cover">
+        <div id="page-container" className="min-h-screen">
             <Image
                 src={`/img/${imageFilename}`}
                 fill={true}
